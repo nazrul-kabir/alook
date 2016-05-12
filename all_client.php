0c4e79aa2c56cb4ec80639bd274e8bc9120c2a27
ozonex <ozonex91@outlook.com>
2016-05-12 23:05:03 +0600

    ok

---
+++ all_client.php
@@ -0,0 +1,102 @@
+<?php
+include './config/config.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Alook Refrigeration CO. Ltd | Our Valuable Clients</title>
+        <?php include './header_script.php'; ?>
+    </head>
+    <body id="boxed">
+        <div class="boxed-wrapper">
+            <?php include './header.php'; ?>  
+            <div class="breadcrumb-wrap">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <h4>OUR VALUABLE CLIENTS</h4>
+                        </div>
+                        <div class="col-sm-6 hidden-xs text-right">
+                            <ol class="breadcrumb">
+                                <li><a href="index.php">HOME</a></li>
+                                <li>CLIENTS</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="divide80"></div>
+                <div class="row">
+                    <div class="col-sm-4 margin30">
+                        <h4>Default template</h4>
+                        <ol>
+                            <li><a href="index.html">Revolution Full-width</a></li>
+                            <li><a href="home-revolution-boxed.html">Revolution Boxed</a></li>
+                            <li><a href="home-revolution-fullscreen.html">Revolution Fullscreen</a></li>
+                            <li><a href="ken-burns.html">Ken burns Slider</a></li>
+                            <li><a href="home-carousel.html">Carousel Slider</a></li>
+                            <li><a href="home-flexslider.html">Flex slider</a></li>    
+                            <li><a href="r5-classic.html">Revolution-5 Carousel classic</a></li> 
+                            <li><a href="r5-gym.html">Revolution-5 Slider Gym</a></li> 
+                            <li><a href="home-parallax.html">Home - Parallax</a></li>
+                            <li><a href="home-video.html">Home -Youtube Video</a></li><li><a href="home-self-video.html">Home - Video(Self-hosted) <span class="label new-label">New</span></a></li>
+                            <li><a href="home-boxed.html">Home - Boxed</a></li>
+                            <li><a href="home-construction.html">Home - Construction</a></li>
+                            <li><a href="home-portfolio.html">Home - Portfolio</a></li>
+                            <li><a href="home-events.html">Home - Events</a></li> 
+
+                        </ol>
+                    </div>
+                    <div class="col-sm-4 margin30">
+                        <h4>Default template</h4>
+                        <ol>
+                            <li><a href="index.html">Revolution Full-width</a></li>
+                            <li><a href="home-revolution-boxed.html">Revolution Boxed</a></li>
+                            <li><a href="home-revolution-fullscreen.html">Revolution Fullscreen</a></li>
+                            <li><a href="ken-burns.html">Ken burns Slider</a></li>
+                            <li><a href="home-carousel.html">Carousel Slider</a></li>
+                            <li><a href="home-flexslider.html">Flex slider</a></li>    
+                            <li><a href="r5-classic.html">Revolution-5 Carousel classic</a></li> 
+                            <li><a href="r5-gym.html">Revolution-5 Slider Gym</a></li> 
+                            <li><a href="home-parallax.html">Home - Parallax</a></li>
+                            <li><a href="home-video.html">Home -Youtube Video</a></li><li><a href="home-self-video.html">Home - Video(Self-hosted) <span class="label new-label">New</span></a></li>
+                            <li><a href="home-boxed.html">Home - Boxed</a></li>
+                            <li><a href="home-construction.html">Home - Construction</a></li>
+                            <li><a href="home-portfolio.html">Home - Portfolio</a></li>
+                            <li><a href="home-events.html">Home - Events</a></li> 
+                        </ol>
+                    </div>
+                    <div class="col-sm-4 margin30">
+
+                        <h4>Real Estate Template</h4>
+                        <ol>
+                            <li><a href="index.html">Home - Slider</a></li>
+                            <li><a href="real-estate-home-map.html">Home - Map</a></li>
+                            <li><a href="r-property-listing.html">Property Listing</a></li>
+                            <li><a href="r-property-detail.html">Property Detail</a></li>
+                            <li><a href="r-agent-listing.html">Agent Listing</a></li>
+                            <li><a href="r-agent-detail.html">Agent Detail</a></li>
+                            <li><a href="r-agency-listing.html">Agency Listing</a></li>
+                            <li><a href="r-agency-detail.html">Agency Detail</a></li>                          
+                            <li><a href="real-estate-pricing.html">Pricing</a></li>
+                            <li><a href="real-estate-faqs.html">Faqs</a></li>         
+                            <li><a href="real-estate-blog.html">Blog</a></li>
+                            <li><a href="real-estate-contact.html">Contact</a></li>
+                            <li><a href="real-estate-login-register.html">Login</a></li>
+                            <li class="active"><a href="r-add-property.html">Add Property</a></li>
+
+                        </ol>
+                        
+                    </div>
+
+                </div>
+            </div>
+            <?php include './footer.php'; ?>
+        </div>
+        <?php include './footer_script.php'; ?>
+    </body>
+</html>
